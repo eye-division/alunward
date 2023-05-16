60265b9ce712297cdd09b814ad39cec86e87c5d8
Alun Ward <a.ward@eyedivision.com>
2023-05-16 15:28:33 +0100

    Update all£

--- Gemfile
+++ Gemfile
@@ -11,7 +11,6 @@ group :jekyll_plugins do
   gem 'jekyll-tagging-related_posts'
   gem 'jekyll-tagging'
   # Oct 2018 - replace jekyll sass gem with sassc
-  # gem 'sassc'
+  gem 'sassc'
   gem "jekyll-cloudinary"
-  gem 'jekyll-postcss'
 end
--- js/main.js
+++ js/main.js
@@ -1,34 +1,13 @@
+(function (window, document) {
+  document.getElementById('toggle').addEventListener('click', function (e) {
+      document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+      document.getElementById('toggle').classList.toggle('x');
+  });
+  })(this, this.document);
+  
 (function($) {
   "use strict"; // Start of use strict
   $(document).ready(function() {
-
-    // Syntax highlighting
-    //hljs.initHighlightingOnLoad();
-
-    // Header
-    var menuToggle = $('#js-mobile-menu').unbind();
-    $('#js-navigation-menu').removeClass("show");
-    menuToggle.on('click', function(e) {
-      e.preventDefault();
-      $('#js-navigation-menu').slideToggle(function(){
-        if($('#js-navigation-menu').is(':hidden')) {
-          $('#js-navigation-menu').removeAttr('style');
-        }
-      });
-    });
-
-    // Drop down v2 - Bourbon
-    $(".dropdown-button").click(function() {
-      var $button, $menu;
-      $button = $(this);
-      $menu = $button.siblings(".dropdown-menu");
-      $menu.toggleClass("show-menu");
-      $menu.children("li").click(function() {
-        $menu.removeClass("show-menu");
-        $button.html($(this).html());
-      });
-    });
-
 
     // Lightbox2 options
     lightbox.option({
@@ -36,15 +15,6 @@
     });
 
 
-    // flexslider
-    $('.flexslider').flexslider({
-      animation: "fade",
-      controlNav: false,
-      directionNav: true,
-      slideshowSpeed: 5000,
-      animationSpeed: 600,
-      touch: true
-    });
 
   }); // end doc ready
 
